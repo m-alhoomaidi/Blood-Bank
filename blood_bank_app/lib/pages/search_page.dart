8bc08e79e74fac934554bfc90db4ef7c7ea8d73a
Ezzalarab Al-Homaidi <ezz2019alarab@gmail.com>
2022-11-13 16:37:27 +0300

    prepare search widgets

--- blood_bank_app/lib/pages/search_page.dart
+++ blood_bank_app/lib/pages/search_page.dart
@@ -1,3 +1,5 @@
+import 'package:blood_bank_app/widgets/search/search_options.dart';
+import 'package:blood_bank_app/widgets/search/search_result.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
@@ -10,9 +12,28 @@ class SearchPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Search Page'),
         centerTitle: true,
+        elevation: 0,
       ),
-      body: const Center(
-        child: Text('Search'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+              ),
+              child: const SearchOptions(),
+            ),
+          ),
+          const Expanded(
+            flex: 2,
+            child: SearchResult(),
+          ),
+        ],
       ),
     );
   }
---
+++ blood_bank_app/lib/widgets/search/search_result.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchResult extends StatelessWidget {
+  const SearchResult({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('result'),
+    );
+  }
+}
