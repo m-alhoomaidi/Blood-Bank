37b4bf545cea6ba54c133e30a8c7ec15d1fa7237
Ezzalarab Al-Homaidi <ezz2019alarab@gmail.com>
2022-11-22 22:57:00 +0300

    edit card body

--- blood_bank_app/lib/widgets/search/doner_card_body.dart
+++ blood_bank_app/lib/widgets/search/doner_card_body.dart
@@ -10,20 +10,43 @@ class DonerCardBody extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        SizedBox(height: 15),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(
-              Icons.phone,
-              size: 30,
+            Container(
+              height: 60,
+              width: 150,
+              decoration: const BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: const Icon(
+                Icons.phone,
+                color: Colors.white,
+                size: 30,
+              ),
             ),
             SizedBox(
-              width: MediaQuery.of(context).size.width * 0.2,
+              width: MediaQuery.of(context).size.width * 0.05,
             ),
-            Icon(
-              Icons.message,
-              size: 30,
+            Container(
+              height: 60,
+              width: 150,
+              decoration: const BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: const Icon(
+                Icons.message,
+                color: Colors.white,
+                size: 30,
+              ),
             ),
           ],
         )
