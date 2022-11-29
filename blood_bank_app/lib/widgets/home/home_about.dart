41405cd599e3d43166f50ac99403a6461497de21
Ezzalarab Al-Homaidi <ezz2019alarab@gmail.com>
2022-11-30 00:45:29 +0300

    home_about create

--- blood_bank_app/lib/widgets/home/home_about.dart
+++ blood_bank_app/lib/widgets/home/home_about.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class HomeAbout extends StatelessWidget {
   const HomeAbout({super.key});
