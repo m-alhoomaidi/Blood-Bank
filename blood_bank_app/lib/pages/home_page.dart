4016e5761a37a89195cd7431eca6deb33594ce7a
Ezzalarab Al-Homaidi <ezz2019alarab@gmail.com>
2022-11-29 23:45:29 +0300

    add home welcome and about sections . empty

--- blood_bank_app/lib/pages/home_page.dart
+++ blood_bank_app/lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:blood_bank_app/widgets/home/home_about.dart';
+import 'package:blood_bank_app/widgets/home/home_welcome.dart';
+
 import 'search_page.dart';
 import '../models/extention.dart';
 import '../shared/style.dart';
@@ -43,25 +46,11 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              color: ePrimColor,
-              child: const HomeCarousel(),
-            ),
-            const Divider(),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
-              child: Text(
-                "إحصائيات",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const HomeCharts(),
-            const SizedBox(height: 20),
+          children: const <Widget>[
+            HomeWelcome(),
+            HomeAbout(),
+
+            // Container(
             // const Padding(
             //   padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
             //   child: Text(
@@ -83,6 +72,24 @@ class HomePage extends StatelessWidget {
             //     ],
             //   ),
             // ),
+            //   padding: const EdgeInsets.symmetric(vertical: 10.0),
+            //   color: ePrimColor,
+            //   child: const HomeCarousel(),
+            // ),
+            // const Divider(),
+            // const Padding(
+            //   padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+            //   child: Text(
+            //     "إحصائيات",
+            //     style: TextStyle(
+            //       fontSize: 18,
+            //       fontWeight: FontWeight.bold,
+            //     ),
+            //   ),
+            // ),
+            // const HomeCharts(),
+
+            SizedBox(height: 20),
           ],
         ),
       ),
---
+++ blood_bank_app/lib/widgets/home/home_about.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeAbout extends StatelessWidget {
+  const HomeAbout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
