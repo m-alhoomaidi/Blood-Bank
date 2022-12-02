47817ce7f3437752b3cdf4ec1144de534b30e9f6
Amran Alameri <amr718173@gmail.com>
2022-12-02 19:42:02 +0300

    updating-the-code-order

--- blood_bank/src/Components/home/greeting-section.jsx
+++ blood_bank/src/Components/home/greeting-section.jsx
@@ -1,13 +1,11 @@
-import { Box, Button, Typography } from "@mui/material";
-import PersonSearchIcon from "@mui/icons-material/PersonSearch";
+import { Box, Typography } from "@mui/material";
 import backgroundHomeImage from "../../Images/blood-donor.jpg";
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
+
 
 export const GreetingSection = (props) => {
   const { children } = props;
@@ -38,27 +36,38 @@ export const GreetingSection = (props) => {
             
     <Paper
       component="form"
-      sx={{ p: '2px 4px',marginTop:"3px", display: 'flex', alignItems: 'center', width: 400 }}
+      sx={{
+        p: '2px 4px',
+        marginTop:"3px",
+        display: 'flex',
+        alignItems: 'center',
+        width: 400 
+        }}
     >
-    <IconButton type="button" sx={{ p: '10px', color: "#c1262e" }} aria-label="search">
-        <SearchIcon />
+    <IconButton type="button"
+     sx={{
+       p: '10px',
+       color: "#c1262e"
+        }} 
+    aria-label="search">
+    <SearchIcon />
     </IconButton>
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      <Divider 
+      sx={{
+        height: 28,
+        m: 0.5 
+        }} 
+    orientation="vertical" />
     
       <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="البحث عن متبرع"
-        inputProps={{ 'aria-label': 'search google maps' }}
+        sx={{
+          ml: 1,
+          flex: 1
+            }}
+      placeholder="البحث عن متبرع"
+      inputProps={{ 'aria-label': 'search google maps' }}
       />
     </Paper>
-          {/* <Button
-            variant="contained"
-            sx={{ backgroundColor: "#c1262e" }}
-            startIcon={<PersonSearchIcon sx={{ marginLeft: "8px" }} />}
-          >
-            {" "}
-            البحث عن متبرع{" "}
-          </Button> */}
         </Box>
       </Box>
     </Box>
