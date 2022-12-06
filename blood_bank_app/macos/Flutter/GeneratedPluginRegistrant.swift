6b24782c42cb0a407197460cbdb54d74c06b1f22
Ezzalarab Al-Homaidi <ezz2019alarab@gmail.com>
2022-12-06 16:47:21 +0300

    add firbase_messaging & flutter_local_notifications

--- blood_bank_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ blood_bank_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,8 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
+import firebase_messaging
+import flutter_local_notifications
 import geolocator_apple
 import location
 import modal_progress_hud_nsn
@@ -17,6 +19,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   ModalProgressHudNsnPlugin.register(with: registry.registrar(forPlugin: "ModalProgressHudNsnPlugin"))
