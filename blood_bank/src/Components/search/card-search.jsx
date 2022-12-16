a95301f4da44015a362b8644074c42d8c72e5007
Abdullahalyfrsy <abdullahalyfrsy@gmail.com>
2022-12-16 04:27:20 +0300

    order

--- blood_bank/src/Components/search/card-search.jsx
+++ blood_bank/src/Components/search/card-search.jsx
@@ -1,53 +1,48 @@
 import React, { useState } from 'react';
-import { Avatar, Box, Card, CardContent, Grid, Typography,Fab,Collapse,CardActions, CardHeader } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
+import { Avatar, Box, Card,Grid, Typography} from '@mui/material';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import Button from '@mui/material/Button';
 import MessageIcon from '@mui/icons-material/Message';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { styled } from '@mui/material/styles';
 const CardSearch = (props) => {    
-    const [expanded, setExpanded] =useState(false);
-    const handleExpandClick = () => {
-      setExpanded(!expanded);
-    };
     return (
     <Box container>
-  <Card
-    sx={{ height: '100%' }}
-    {...props}
-  >
-    <Grid container spacing={3}>
-      <Grid item>
-           <Avatar
-            sx={{backgroundColor: 'error.main',marginRight:"20px"}}>
-            A+
-          </Avatar>
-       </Grid>
-       <Grid item>
-             <Typography sx={{marginTop:"8px"}}>عبدالله احمد</Typography>
-        </Grid>
-        <Grid item xs={4}></Grid>
-    </Grid>
-    
-    <Grid container spacing={8} justifyContent="center">
-      <Grid item >
-      <Typography sx={{margin:"-10px 60px 0px 0px"}}>المنطقة - السبل</Typography>
-        </Grid>
-        <Grid item xs={2}></Grid>
-        </Grid>
-        <Box container sx={{margin:"40px 20px"}}>
-        <Grid container spacing={8} >
-      <Grid item >
-      <Button variant="contained" sx={{background:"green"}} ><LocalPhoneIcon /></Button>
-        </Grid>
-        <Grid item >
-      <Button variant="contained" sx={{background:"yellow"}}><MessageIcon /></Button>
-        </Grid>
-        </Grid>
-        </Box>
-  </Card>
-  </Box>
-    );
+       <Card sx={{ height: '100%' }}>
+          <Grid container spacing={3}>
+            <Grid item>
+                <Avatar
+                   sx={{backgroundColor: 'error.main',marginRight:"20px"}}>
+                     A+
+                </Avatar>
+             </Grid>
+            <Grid item>
+                <Typography sx={{marginTop:"8px"}}>عبدالله احمد</Typography>
+            </Grid>
+            <Grid item xs={4}></Grid>
+            </Grid>
+          <Grid container spacing={8} justifyContent="center">
+           <Grid item >
+              <Typography sx={{margin:"-10px 60px 0px 0px"}}>المنطقة - السبل</Typography>
+            </Grid>
+            <Grid item xs={2}></Grid>
+          </Grid>
+          <Box container sx={{margin:"40px 20px"}}>
+            <Grid container spacing={8} >
+            <Grid item >
+               <Button variant="contained" sx={{background:"green", "&:hover": {
+                  backgroundColor: "green",
+                  textDecoration: "none",
+                },}} ><LocalPhoneIcon /></Button>
+            </Grid>
+            <Grid item >
+                <Button variant="contained" sx={{background:"yellow", "&:hover": {
+                  backgroundColor: "yellow",
+                  textDecoration: "none",
+                },}}><MessageIcon /></Button>
+             </Grid>
+            </Grid>
+          </Box>
+       </Card>
+    </Box>
+  );
 };
 export default CardSearch;
--- blood_bank/src/Components/search/search-userblood.jsx
+++ blood_bank/src/Components/search/search-userblood.jsx
@@ -18,51 +18,40 @@ export const  SearchUserBlood =()=> {
         { label: 'AB-'},
         { label: 'O-'},
       ];
-
-    const [Stateid,setStateid]=useState([]);
+const [Stateid,setStateid]=useState([]);
       const [City,setCity]=useState([]);
       const [StateCityid,setStateCityid]=useState('');
   
-      const HandleCountry = (event,e)=>{
+const HandleCountry = (event,e)=>{
           const getStateid=e.id;
           const getCitydata=Countryes.find(country =>country.id===getStateid).city;
           setCity(getCitydata);
           setStateid(getStateid);
-         console.log(getStateid);
-        
-      }
-      const HandleState=(e) =>{
-                const Stateid=e.target.value;
-                setStateCityid(Stateid);
-            }
-
-   
-    return (
+         console.log(getStateid);}
+const HandleState=(e) =>{
+        const Stateid=e.target.value;
+        setStateCityid(Stateid);}
+ return (
         <Box container sx={{marginTop:"80px"}}>  
-          <Paper  sx={{
-        p: 2,
-        margin: 'auto',
-        flexGrow: 1,
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-      }}>            
-             <Grid container spacing={3} justifyContent="center">     
+          <Paper  sx={{p: 2,margin: 'auto',flexGrow: 1,
+                   backgroundColor: (theme) =>
+                   theme.palette.mode === 'dark' ? '#1A2027' : '#fff'}}>            
+                 <Grid container spacing={3} justifyContent="center">     
                     <Grid item>
-                    <Autocomplete
-                    disablePortal
-                        id="Country"
-                        onChange={HandleCountry}
-                       
-                        options={Countryes}
+                       <Autocomplete
+                         disablePortal
+                         id="Country"
+                         onChange={HandleCountry}
+                         options={Countryes}
                          getOptionLabel={(Countryes) => Countryes.name}
                          sx={{ width: 300 }}
                          renderOption={(props,option) =><Box component="li" key={option.id} {...props}>{option.name}</Box> }
                          renderInput={(params) => <TextField {...params} variant="outlined" label="المحافظة"  />}
-                    />
-                    </Grid>
-                    <Grid item>
-                    <Autocomplete
-                    disablePortal
+                       />
+                      </Grid>
+                 <Grid item>
+                        <Autocomplete
+                        disablePortal
                         id="City"
                         onChange={HandleState}
                         options={City}
@@ -70,35 +59,33 @@ export const  SearchUserBlood =()=> {
                          sx={{ width: 300 }}
                          renderOption={(props,option) =><Box component="li" key={option.id} {...props}>{option.name}</Box> }
                          renderInput={(params) => <TextField {...params} variant="outlined" label="المديرية" />}
-                    />
+                        />
                     </Grid>
-                    <Grid item  >
+                 <Grid item  >
                     <Autocomplete
                         disablePortal
                         id="combo-box-demo"
                         options={TypeBlood}
                          sx={{ width: 300 }}
                          renderInput={(params) => <TextField {...params} label="فصيلة الدم" variant="outlined"/>}
-                    />
+                       />
+                 </Grid>
+                    <Grid item>
+                       <Button variant="contained" sx={{width:"150px",marginTop:"8px"}}>بــحــث</Button>
+                    </Grid>
+               </Grid>
+            </Paper> 
+                <Grid container spacing={3} justifyContent="center" 
+                    sx= {{marginTop:"20px"}} > 
+                    <Grid item>
+                       <CardSearch/>
                     </Grid>
                     <Grid item>
-                    <Button variant="contained" sx={{width:"150px",marginTop:"8px"}}>بــحــث</Button>
+                      <CardSearch/>
                     </Grid>
-                </Grid>
-                </Paper> 
-                <Grid container spacing={3} justifyContent="center" sx={{marginTop:"20px"}}> 
-                <Grid item>
-                    <CardSearch/>
-
-                </Grid>
-                <Grid item>
-                    <CardSearch/>
-
-                </Grid>
-                <Grid item>
-                    <CardSearch/>
-
-                </Grid>
+                    <Grid item>
+                      <CardSearch/>
+                    </Grid>
                 </Grid>
         </Box>
     );};
