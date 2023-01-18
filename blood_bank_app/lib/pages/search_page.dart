50c9425ba3feff0e49a0b2ef4e0d2b037482f987
Ezzalarab Al-Homaidi <ezz2019alarab@gmail.com>
2023-01-18 16:27:54 +0300

    set textscaleFactor on search page

--- blood_bank_app/lib/pages/search_page.dart
+++ blood_bank_app/lib/pages/search_page.dart
@@ -10,32 +10,35 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search Page'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      backgroundColor: eBackGroundColor,
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 190,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
+    return MediaQuery(
+      data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Search Page'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        backgroundColor: eBackGroundColor,
+        body: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 190,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
                 ),
+                child: SearchOptions(),
               ),
-              child: SearchOptions(),
             ),
-          ),
-          const Expanded(
-            child: SearchResult(),
-          ),
-        ],
+            const Expanded(
+              child: SearchResult(),
+            ),
+          ],
+        ),
       ),
     );
   }
