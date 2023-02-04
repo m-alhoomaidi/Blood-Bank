3bdfaf7337acdb76b2f6f920656dc42f58bde8b8
Ezzalarab Al-Homaidi <ezz2019alarab@gmail.com>
2023-02-04 18:02:08 +0300

    add collections images

--- blood_bank_app/lib/presentation/pages/splash_screen.dart
+++ blood_bank_app/lib/presentation/pages/splash_screen.dart
@@ -5,7 +5,6 @@ import 'package:blood_bank_app/presentation/resources/color_manageer.dart';
 import 'package:blood_bank_app/presentation/widgets/common/dialog_lottie.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:page_transition/page_transition.dart';
 
 class SplashScreen extends StatelessWidget {
