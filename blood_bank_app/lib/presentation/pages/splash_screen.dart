a823c2976470d30a8645fa52d3849d761fa23e35
Abdullahalyfrsy <abdullahalyfrsy@gmail.com>
2023-02-04 22:44:52 +0300

    problem

--- blood_bank_app/lib/presentation/pages/splash_screen.dart
+++ blood_bank_app/lib/presentation/pages/splash_screen.dart
@@ -6,6 +6,10 @@ import 'package:blood_bank_app/presentation/resources/color_manageer.dart';
 import 'package:blood_bank_app/presentation/widgets/common/dialog_lottie.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+<<<<<<< HEAD
+=======
+import 'package:page_transition/page_transition.dart';
+>>>>>>> c190121177ca54752925eb648e5a71454f0e30fb
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
